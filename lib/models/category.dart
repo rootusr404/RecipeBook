a0194317fb6fabd02cf79f50f51a66cad3066f02
rootusr404 <root404user@gmail.com>
2026-07-30 06:57:23 -0400

    Fix lint warnings and test flakiness

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -5,23 +5,28 @@ class Category {
   final String name;
   final IconData icon;
 
-  const Category({required this.id, required this.name, required this.icon});
+  const Category({
+    required this.id,
+    required this.name,
+    required this.icon,
+  });
 
   factory Category.fromJson(Map<String, dynamic> json) {
-  return Category(
-    id: json['id'] as String,
-    name: json['name'] as String,
-    // ignore: non_const_argument_for_const_parameter
-    icon: IconData(
-      json['iconCodePoint'] as int,
-      fontFamily: 'MaterialIcons',
-    ),
-  );
-}
+    final int codePoint = json['iconCodePoint'] as int;
+   // ignore: non_const_argument_for_const_parameter
+final IconData resolvedIcon = IconData(codePoint, fontFamily: 'MaterialIcons');
+    return Category(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      icon: resolvedIcon,
+    );
+  }
 
-Map<String, dynamic> toJson() {
-  return {'id': id, 'name': name, 'iconCodePoint': icon.codePoint};
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'iconCodePoint': icon.codePoint,
+    };
+  }
 }
-
-}
-
